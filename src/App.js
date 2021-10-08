41b9282d534deeb57c30381558dade9e544cc700
Benny Luo <bennyluo95@gmail.com>
2021-10-07 21:14:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {useState, useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const AlphaInput = () =>{
+
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+  return(
+    <div>
+      {alphabet.map( a => {
+        return (
+          <div>
+          <label>{a}</label>
+          <input
+            type="text" 
+            id={a}
+            maxlength="10"
+          />
+        </div>
+        )
+      })}
     </div>
-  );
+    
+  )
 }
+
+const App = () => {
+
+  return(
+    <div>
+      <h2>alphatype</h2>
+      <AlphaInput/>
+    </div>
+  )
+} 
 
 export default App;
