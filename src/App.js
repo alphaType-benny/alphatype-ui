76ac66066633d054eef432759e230b12c02ee5a2
Benny Luo <bennyluo95@gmail.com>
2021-10-08 16:07:57 -0400

    added timer

--- src/App.js
+++ src/App.js
@@ -6,20 +6,71 @@
 //Style page
 //About (multipage)
 
-import react, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const AlphaInput = () =>{
-
+  const [start, setStart] = useState("")
   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-  const displayResult = () =>{
-    console.log("finsih")
+  const timer = () =>{
+    let offset, clock, interval
+
+    const start = () =>{
+      offset = Date.now()
+    }
+
+    const delta = () =>{
+      const now = Date.now()
+      const d = now - offset
+      offset = now
+      return d
+    }
+
+    const update = () =>{
+      clock += delta()
+      render()
+    }
+
+    const render = () =>{
+      console.log(clock);
+    }
+
+    interval = setInterval(update(), 1)
+
+    const stop = () => {
+      if (interval) {
+        clearInterval(interval);
+        interval = null;
+      }
+    }
+
+    const reset = () =>{
+      clock = 0 
+      render(0)
+    }
+
+    return (
+      {start, stop, reset}
+    )
   }
 
   const spellCheck = (idx, letter, value) =>{
-      if (value === letter) {
-        return letter!=="z" ? document.getElementById(idx+1).focus() : displayResult()
+
+    if (letter === "a"){
+      setStart(Date.now)
+      const interval = setInterval(() => {
+        console.log("log!");
+      }, 1000);
+      //clearInterval()
+    }
+
+    if (value === letter) {
+      if(letter === "b"){
+        let finish = Date.now()
+        console.log(`Finished in ${(finish-start)/1000}`)
       }
+      return letter!=="z" ? document.getElementById(idx+1).focus() : null
+    }
   }
 
   return(
@@ -28,7 +79,7 @@ const AlphaInput = () =>{
         return (
           <span key={idx}>
           {idx===13?<br/>:""}
-          <label>{a}</label>
+          <label> {a} </label>
           <input
             type="text" 
             id={idx}
@@ -44,6 +95,8 @@ const AlphaInput = () =>{
 }
 
 const App = () => {
+
+
 
   return(
     <div>
