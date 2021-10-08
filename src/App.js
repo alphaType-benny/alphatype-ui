b278924bf9650757b00979cfd51437a44757936e
Benny Luo <bennyluo95@gmail.com>
2021-10-07 22:30:46 -0400

    added input fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+
+//Add timer on keypress
+//Stop timer when z is pressed
+//Login page (username)
+//Top 5 highscore players
+//Style page
+//About (multipage)
+
 import react, {useState, useEffect} from 'react'
 
 const AlphaInput = () =>{
 
   const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+  const displayResult = () =>{
+    console.log("finsih")
+  }
+
+  const spellCheck = (idx, letter, value) =>{
+      if (value === letter) {
+        return letter!=="z" ? document.getElementById(idx+1).focus() : displayResult()
+      }
+  }
+
   return(
     <div>
-      {alphabet.map( a => {
+      {alphabet.map( (a, idx) => {
         return (
-          <div>
+          <span key={idx}>
+          {idx===13?<br/>:""}
           <label>{a}</label>
           <input
             type="text" 
-            id={a}
-            maxlength="10"
+            id={idx}
+            maxLength="1"
+            onChange={(e)=>spellCheck(idx, a, e.target.value)}
+            style={{width: "10px"}}
           />
-        </div>
+        </span>
         )
       })}
     </div>
-    
   )
 }
 
