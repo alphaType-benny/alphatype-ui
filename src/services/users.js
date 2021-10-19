413b781b7e80e0d72d8ea4631abae3766f953bb0
Benny Luo <bennyluo95@gmail.com>
2021-10-18 22:55:17 -0400

    added login ui

---
+++ src/services/users.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+const baseUrl = '/api/users'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return (response.data)
+}
+
+const postResult = (body) => {
+    const request = axios.post(baseUrl, body)
+    return request.then(response => (response.data))
+}
+
+const resultService = {getAll, postResult}
+  
+export default resultService
